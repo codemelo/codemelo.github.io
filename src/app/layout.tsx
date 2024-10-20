18a911afdf64b3979bd858ff4e60dc26492af941
codemelo <yello28@proton.me>
2024-10-20 12:02:51 +1100

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Carmelo - Software Developer',
-  description: 'Carmelo - Software Developer',
+  title: 'Carmelo | Software Developer',
+  description: 'Carmelo | Software Developer',
 }
 
 export default function RootLayout({
